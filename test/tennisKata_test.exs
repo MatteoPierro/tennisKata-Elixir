722adf2e8189d9ad3d3c84fe32f0e69ccc73d9fa
Matteo <pierromatteo@gmail.com>
2015-10-22 21:53:45 +0200

    player one scored one point

--- test/tennisKata_test.exs
+++ test/tennisKata_test.exs
@@ -1,7 +1,12 @@
 defmodule Game do
   def start do
+    game(0, 0)
+  end
+
+  def game(player_one, player_two) do
     receive do
-      {:get_score, pid} -> send pid, {:score, [0, 0]}
+      {:get_score, pid} -> send pid, {:score, [player_one, player_two]}
+      :player_one -> game(15, player_two)
     end
   end
 end
@@ -14,4 +19,12 @@ defmodule TennisKataTest do
   	send game, {:get_score, self}
   	assert_receive {:score, [0, 0]}
   end
+
+  test "player scored one point" do
+    game = spawn_link(Game, :start, [])
+    send game, :player_one
+    send game, {:get_score, self}
+    assert_receive {:score, [15, 0]}
+  end
+
 end
