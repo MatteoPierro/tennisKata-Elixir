633c41be9822ce9a2617cf26b5a972a3f784a151
Matteo <pierromatteo@gmail.com>
2015-10-22 21:40:57 +0200

    add zero all case

--- test/tennisKata_test.exs
+++ test/tennisKata_test.exs
@@ -1,8 +1,17 @@
+defmodule Game do
+  def start do
+    receive do
+      {:get_score, pid} -> send pid, {:score, [0, 0]}
+    end
+  end
+end
+
 defmodule TennisKataTest do
   use ExUnit.Case
-  doctest TennisKata
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "it returns zero all" do
+  	game = spawn_link(Game, :start, [])
+  	send game, {:get_score, self}
+  	assert_receive {:score, [0, 0]}
   end
 end
