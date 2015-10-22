97f99bb7e2c98c31e044917b03a367d3041cc118
Matteo <pierromatteo@gmail.com>
2015-10-22 22:59:43 +0200

    a player lose the match

--- test/tennisKata_test.exs
+++ test/tennisKata_test.exs
@@ -1,21 +1,28 @@
 defmodule Game do
   def start do
-    game(0, 0)
+    game([0, 0])
   end
 
-  def game(player_one, player_two) do
+  def game([player_one, player_two]) do
     receive do
       {:get_score, pid} -> send pid, {:score, [player_one, player_two]}
-      :player_one -> game(next_player_score(player_one), player_two)
-      :player_two -> game(player_one, next_player_score(player_two))
+      :player_one -> game(next_score(:player_one, [player_one, player_two]))
+      :player_two -> game(next_score(:player_two, [player_one, player_two]))
     end
   end
 
-  defp next_player_score(current_player_score) do
-    case current_player_score do
-      40 -> :win
-      30 -> 40
-      _  -> current_player_score + 15
+  defp next_score(player, [player_one_score, player_two_score]) do
+    case player do
+      :player_one ->  add_point([player_one_score, player_two_score])
+      :player_two ->  Enum.reverse(add_point([player_two_score, player_one_score]))               
+    end
+  end
+
+  defp add_point([winner_score, loser_score]) do
+    case [winner_score, loser_score] do
+        [40, _] -> [:win, :lose]
+        [30, _] -> [40, loser_score]
+          _     -> [winner_score + 15, loser_score]
     end
   end
 end
@@ -83,7 +90,17 @@ defmodule TennisKataTest do
     send game, :player_one
     send game, :player_one
     send game, {:get_score, self}
-    assert_receive {:score, [:win, 0]}
+    assert_receive {:score, [:win, _]}
+  end
+
+  test "a player lose the match" do
+    game = spawn_link(Game, :start, [])
+    send game, :player_two
+    send game, :player_two
+    send game, :player_two
+    send game, :player_two
+    send game, {:get_score, self}
+    assert_receive {:score, [:lose, :win]}
   end
 
 end
